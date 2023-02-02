f1645128a37ef690a1fb49a421e1fc75b1853ab2
muhammed <ms.turfan@gmail.com>
2023-02-01 20:39:47 -0800

    Initial Commit

---
+++ src/Last_Dance/Github.java
@@ -0,0 +1,7 @@
+package Last_Dance;
+
+public class Github {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/Last_Dance/Reverse_String.java
@@ -0,0 +1,7 @@
+package Last_Dance;
+
+public class Reverse_String {
+    public static void main(String[] args) {
+        System.out.println("siuuu");
+    }
+}
