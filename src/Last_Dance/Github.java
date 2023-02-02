bc4661f6a961c0f0724109022aec49610f9d02e3
muhammed <ms.turfan@gmail.com>
2023-02-01 21:33:16 -0800

    Last Change 2/1/2023 9.33PM

--- src/Last_Dance/Github.java
+++ src/Last_Dance/Github.java
@@ -4,5 +4,6 @@ public class Github {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("last change");
+        System.out.println("i found a bug");
     }
 }
--- src/Last_Dance/ReverseString.java
+++ src/Last_Dance/ReverseString.java
@@ -4,6 +4,7 @@ public class ReverseString {
 
     public static void main(String[] args) {
         System.out.println("Reverse String");
+        System.out.println("new");
 
     }
 }
